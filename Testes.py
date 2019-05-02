58fb19960179cd77dcacfd1bac2547b223862e86
GuilhermeCunha <guiscunha@gmail.com>
2019-05-02 04:27:08 -0300

    com BD

--- Testes.py
+++ Testes.py
@@ -2,10 +2,12 @@ from Banco import Banco
 import ChatBot
 import Funções
 banco = Banco()
-usuario = {'first_name': "Gabriel", 'chatid': "123456789", 'ra': "192.123456"}
-banco.inserirUsuario(usuario)
+#usuario = {'first_name': "Gabriel", 'chatid': "123456789", 'ra': "192.123456"}
+#banco.inserirUsuario(usuario)
 #print(banco.numeroUsuarios())
 #print(banco.listarUsuarios())
-print(banco.listarUsuarios())
+#print(banco.listarUsuarios())
+banco.apagarCollection()
+print(banco.numeroUsuarios())
 
 
