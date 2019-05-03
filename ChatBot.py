fd82fe7eeba7c76b1dacadae6e92c1fa6ef77152
GuilhermeCunha <guiscunha@gmail.com>
2019-05-02 22:37:09 -0300

    V3.0

---
+++ ChatBot.py
@@ -0,0 +1,51 @@
+import Funções
+from Banco import Banco
+from unidecode import unidecode  # Tirar acentos de uma string
+import pymongo # Acessar banco de dados
+#import sys # Descobrir qual o sistema operacional
+#import subprocess as s # Não sei ainda
+
+class Chatbot():
+    def __init__(self, nome, banco):
+        self.nome = nome
+        self.banco = banco
+
+    def escuta(self, frase=None):
+        if frase == None:
+            return 0
+
+        frase = str(frase)
+        frase = frase.lower()
+        frase = unidecode(frase)
+        return frase
+
+    def pensa(self, primeiroNome, chatID, frase):
+        if '/start' in frase:
+            return "Seja bem vindo ao MinhaAula!\n Digite seu RA para finalizar a criação da sua conta!"
+        if '/consultar' in frase:
+            texto = str(frase)
+            mensagem = texto.split(" ")
+            return Funções.resposta(mensagem[1])
+        if '162.' in frase:
+            usuario = {'first_name': primeiroNome, 'chatid': str(chatID), 'ra': frase}
+            if self.banco.acharUsuario(usuario):
+                return "Conta já cadastrada"
+            self.banco.inserirUsuario(usuario)
+            return "Conta cadastrada com sucesso\n\nPara saber quais as funções disponíveis, escreva: /funcoes"
+        if '/minhasala' in frase:
+            ra = self.banco.acharRAporID(chatID)
+            if ra:
+                return Funções.resposta(ra)
+            else:
+                return "Conta não encontrada"
+        if '/funcoes' in frase:
+            return "/minhasala   Para receber onde será a sua sala\n/consultar <RA>   Para consultar a sala de um RA não vinculado a conta\n/criadopor   Para saber quem é meu criador"
+        if '/contascadastradas' in frase:
+            return self.banco.listarUsuarios()
+        if '/criadopor' in frase:
+            return "Criador"
+
+            return
+        return 'Função não configurada'
+
+    #def fala(self,chatID, frase):
---
+++ Testes.py
@@ -0,0 +1,13 @@
+from Banco import Banco
+import ChatBot
+import Funções
+banco = Banco()
+#usuario = {'first_name': "Gabriel", 'chatid': "123456789", 'ra': "192.123456"}
+#banco.inserirUsuario(usuario)
+#print(banco.numeroUsuarios())
+#print(banco.listarUsuarios())
+#print(banco.listarUsuarios())
+banco.apagarCollection()
+print(banco.numeroUsuarios())
+
+
