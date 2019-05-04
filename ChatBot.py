bdd679ba920e8e9a95693efee21dd9b2d7622a6e
GuilhermeCunha <guiscunha@gmail.com>
2019-05-04 16:13:23 -0300

    V4.0 com ajustes

--- ChatBot.py
+++ ChatBot.py
@@ -20,7 +20,7 @@ class Chatbot():
 
     def pensa(self, primeiroNome, chatID, frase):
 
-        if '/start' in frase:
+        if '/start' == frase:
             return "Seja bem vindo ao MinhaAula!\n Digite seu RA para finalizar a criação da sua conta!"
 
         if '/consultar' in frase:
@@ -28,23 +28,23 @@ class Chatbot():
             mensagem = texto.split(" ")
             return Funções.resposta(mensagem[1])
 
-        if '162.' in frase:
+        if '162.' in frase and len(frase) == 10:
             usuario = {'first_name': primeiroNome, 'chatid': str(chatID), 'ra': frase}
             if self.banco.acharUsuario(usuario):
                 return "Conta já cadastrada"
             self.banco.inserirUsuario(usuario)
             return "Conta cadastrada com sucesso\n\nOBS: Estou configurado para enviar mensagens automáticas avisando onde será a sua aula às 12:00 de todos os dias da semana, caso queira saber mais sobre a ativação/desativação deste serviço, escreva: /sabersobremensagens\n\nPara saber quais as funções disponíveis, escreva: /comandos"
-        if '/sabersobremensagens' in frase:
+        if '/sabersobremensagens' == frase:
             return "Ao cadastrar uma conta em meu sistema, estou configurado para te enviar onde será sua sala em todos os dias úteis da semana às 12:00.\n\n" \
                    "Caso queira desabilitar este serviço, e quem sabe reativálo futuramente, consulte o comando: /comandos."
-        if '/minhasala' in frase:
+        if '/minhasala' == frase:
             ra = self.banco.acharRAporID(chatID)
             if ra:
                 return Funções.resposta(ra)
             else:
                 return "Conta não encontrada"
 
-        if '/desativarmensagensautomaticas' in frase:
+        if '/desativarmensagensautomaticas' == frase:
             print("/autoMsgsOF")
             usuario = {'first_name': primeiroNome, 'chatid': str(chatID)}
             if self.banco.desativarMensagensAutomaticas(usuario):
@@ -52,7 +52,7 @@ class Chatbot():
             else:
                 return "Algum erro ocorreu"
 
-        if '/ativarmensagensautomaticas' in frase:
+        if '/ativarmensagensautomaticas' == frase:
             print("/autoMsgsON")
             usuario = {'first_name': primeiroNome, 'chatid': str(chatID)}
             print(usuario)
@@ -61,7 +61,7 @@ class Chatbot():
             else:
                 return "Algum erro ocorreu"
 
-        if '/comandos' in frase:
+        if '/comandos' == frase:
             return "/minhasala   Para receber onde será a sua sala" \
                    "\n/consultar <RA>   Para consultar a sala de um RA não vinculado a conta" \
                    "\n/descadastrar   Para remover a sua conta do nosso sistema" \
@@ -69,10 +69,10 @@ class Chatbot():
                    "\n/desativarmensagensautomaticas   Para desativar as mensagens automáticas às 12:00"\
                    "\n/ativarmensagensautomaticas   Para ativar as mensagens automáticas às 12:00"
 
-        if '/contascadastradas' in frase:
+        if '/contascadastradas' == frase:
             return self.banco.listarUsuarios()
 
-        if '/descadastrar' in frase:
+        if '/descadastrar' == frase:
             usuario = {'chatid': str(chatID)}
             print(usuario)
             if self.banco.removerUsuario(usuario):
@@ -80,7 +80,7 @@ class Chatbot():
                 return "Conta deletada com sucesso"
             return "Esta conta não foi encontrada"
 
-        if '/criadopor' in frase:
+        if '/criadopor' == frase:
             return "Criador"
 
         return
